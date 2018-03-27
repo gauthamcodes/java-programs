2c62743e14dbb200ba1eca8bc07d2d96c88218a4
Gautham Manivannan <gautham.manivannan@ascendlearning.com>
2018-03-27 13:12:35 +0530

    commit

--- samplejava/src/main/java/com/gautham/samplejava/App.java
+++ samplejava/src/main/java/com/gautham/samplejava/App.java
@@ -1,15 +1,17 @@
 package com.gautham.samplejava;
 
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.stream.IntStream;
 
 /**
  * Hello world!
  *
  */
 public class App {
-	public static void main( String[] args )
-    {
-        
-    }
+	public static void main(String[] args) {
+		System.out.println("Number 27 is prime number or not :" + isPrime(27));
+	}
+
+	private static boolean isPrime(int number) {
+		return number > 1 && IntStream.range(2, number).noneMatch(index -> number % index == 0);
+	}
 }
